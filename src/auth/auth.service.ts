7ed677f9373b16c7f91f6d121c6318d8cd7a99c5
janaikerana <janaikerana@gmail.com>
2022-09-13 10:11:29 +0100

    Prisma Modules

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
 //Bussines Logic
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable({})
 export class AuthService {
+    constructor(private prisma: PrismaService){}
     
     signup(){ 
         return {msg: 'i am signed up'};
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { url } from 'inspector';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+    constructor() {
+        super({
+            datasources: {
+                db: {
+                    url: "mysql://root:q1w2e3r4t5@localhost:3306/nest?connection_limit=5"
+                },
+            },
+        });
+    }
+}
